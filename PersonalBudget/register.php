1fc1fa452c371fe48ee54f388d28149308f47718
adrian.lopez.perez <adrian.lopez.perez@56849572-78a7-5838-543a-b52b2454f6db>
2010-11-03 18:50:29 +0000

    Version 0.1

--- PersonalBudget/register.php
+++ PersonalBudget/register.php
@@ -1,8 +1,29 @@
 <?php
-$fail = false;
-if(!isset ($_REQUEST["name"]))
-   $fail = true;
-if(!isset ($_REQUEST["lastName"]))
+@include 'config.php';
+
+if(isset ($_REQUEST["register"])){
+   
+   $fail = false;
+   if(empty($_REQUEST["name"])){
+      $fail = true;
+      $errorMsg[0] = "Name not specified";
+   }
+   if(empty ($_REQUEST["lastName"])){
+      $fail = true;
+      $errorMsg[count($errorMsg)] = "Last name not specified";
+   }
+   if(empty ($_REQUEST["email"])){
+      $fail = true;
+      $errorMsg[count($errorMsg)] = "Email not specified";
+   }
+   if(empty ($_REQUEST["password"])){
+      $fail = true;
+      $errorMsg[count($errorMsg)] = "Password not specified";
+   }
+   
+   
+
+}
 
 ?>
 <!DOCTYPE HTML>
@@ -18,14 +39,24 @@ if(!isset ($_REQUEST["lastName"]))
     </header>
     <article>
        <p>Your personal budget!</p>
+       <?php
+         if(isset ($errorMsg)){
+            echo "<ul>";
+            foreach ($errorMsg as $error){
+               echo "<li>".$error."</li>";
+            }
+            echo "</ul>";
+         }
+       ?>
        <form action="register.php" method="POST">
           <fieldset>
              <legend>Personal data:</legend>
              <p><label>Name: <input type="text" name="name"/></label></p>
              <p><label>Last name: <input type="text" name="lastName"/></label></p>
-             <p><label>Email: <input type="text" name="email"/></label></p>
+             <p><label>Email: <input type="text" name="email" value="<?php echo $_REQUEST['email']; ?>"/></label></p>
              <p><label>Password: <input type="password" name="password"/></label></p>
              <p><button>Create account</button></p>
+             <input type="hidden" name="register" value="true"/>
           </fieldset>
        </form>
     </article>
