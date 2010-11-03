e50d4cfa92fe6e68ccd09e0dd19bdc4acc9454ad
adrian.lopez.perez <adrian.lopez.perez@56849572-78a7-5838-543a-b52b2454f6db>
2010-11-03 17:32:41 +0000

    Bad creation folder

---
+++ trunk/PersonalBudget/login.php
@@ -0,0 +1,20 @@
+<?php
+if(isset ($_REQUEST["email"]) && isset ($_REQUEST["password"])){
+   switch ($_REQUEST["email"]) {
+      case "adrijardi@gmail.com":
+         echo "Hi adrijardi";
+         break;
+      case "einevea@gmail.com":
+         echo "Hi einevea";
+         break;
+
+      default:
+         echo "I don't know you";
+         break;
+   }
+   
+}else{
+   header("HTTP/1.1 403 Forbidden");
+   header("Location: index.php");
+}
+?>
